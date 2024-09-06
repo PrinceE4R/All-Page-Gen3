d9b86761ce6f665a918a5f5334bc96bd191535df
PrinceE4R <100913311+PrinceE4R@users.noreply.github.com>
2024-09-06 11:19:47 +0530

    Delete js directory

--- js/rectangular-drawer.js
+++
@@ -1,8 +0,0 @@
-function drawDrawer2() {
-    let drawer2 = document.querySelector('.rectangular-drawer');
-    if (drawer2.style.transform === "translate(0%, -5%)") {
-        drawer2.style.transform = "translate(-90%, -5%)";
-    } else {
-        drawer2.style.transform = "translate(0%, -5%)";
-    }
-}
