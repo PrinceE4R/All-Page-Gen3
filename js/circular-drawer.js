034c2839abf81c978655b3c96eaa688647830354
PrinceE4R <100913311+PrinceE4R@users.noreply.github.com>
2024-09-10 14:17:57 +0530

    Add files via upload

--- js/circular-drawer.js
+++ js/circular-drawer.js
@@ -1,7 +1,7 @@
 function drawDrawer() {
     let drawer = document.querySelector('.circular-drawer');
     if (drawer.style.transform === "translate(40%, -50%)") {
-        drawer.style.transform = "translate(90%, -50%)";
+        drawer.style.transform = "translate(92%, -50%)";
     } else {
         drawer.style.transform = "translate(40%, -50%)";
     }
